f02d28acd22ad2b57bc33ed47347643d9b259946
John <azxenon3@gmail.com>
2025-02-04 19:50:14 +0800

    autoencoder init

--- run_train.sh
+++ run_train.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-python chempropIRZenodo/chempropIR/train.py \
+time python chempropIRZenodo/chempropIR/train.py \
   --gpu 0 \
   --data_path chempropIRZenodo/trained_ir_model/computed_model/test_full.csv \
   --dataset_type spectra \
-  --save_dir ./output \
-  --config_path chempropIRZenodo/trained_ir_model/recommended_config.json
+  --save_dir ./output/model \
+  --config_path chempropIRZenodo/trained_ir_model/recommended_config.json \
+  --frzn_mpn_checkpoint model/test/model_0.pt
+
+# if we don't want to seperate data auto, assign path
+# --separate_val_path \
+# --separate_test_path \
--- test.py
+++ test.py
@@ -2,15 +2,18 @@ import pandas as pd
 
 
 # full 是答案
-path = ["../chemprop-IR_Zenodo/trained_ir_model/computed_model/test_preds.csv",
-        "../chemprop-IR_Zenodo/trained_ir_model/computed_model/test_full.csv",
-        "../chemprop-IR_Zenodo/trained_ir_model/computed_model/test_smiles.csv",
-        "../chemprop-IR_Zenodo/trained_ir_model/computed_model/computed_spectra.csv",
-        "./output/fingerprint_preds.csv"]
+path = [
+    "./chempropIRZenodo/trained_ir_model/computed_model/test_preds.csv",
+    "./chempropIRZenodo/trained_ir_model/computed_model/test_full.csv",
+    "./chempropIRZenodo/trained_ir_model/computed_model/test_smiles.csv",
+    "./chempropIRZenodo/trained_ir_model/computed_model/computed_spectra.csv",
+    "./output/fingerprint_preds.csv",
+]
 
-df = pd.read_csv(path[4])
+df = pd.read_csv(path[3])
 
 print(df.head())
-# print(df.shape)
+print(df.shape)
 # print(df[df['smiles'] == 'CCCC(C)Oc1nc(N)c2nc(OC)n(CCCCC3CCCNC3)c2n1'])
 # print(df.iloc[0])
+
